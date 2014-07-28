f81ccbb83df7c46930cab285e6e12ebbcabdf5d9
sanderjo <sander.jonkers+github@gmail.com>
2014-07-28 20:44:19 +0200

    Comment added

--- solarmanpv-oneliner.sh
+++ solarmanpv-oneliner.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-curl --silent --cookie-jar /tmp/cookies -o /dev/null 'http://www.solarmanpv.com/portal/Terminal/TerminalMain.aspx?pid=6067' && \
-curl --cookie "ASP.NET_SessionId=`awk ' END { print $NF }' /tmp/cookies`"  'http://www.solarmanpv.com/portal/AjaxService.ashx?ac=upTerminalMain&psid=6067'
+# in this case, get the raw information for pid=5432; fill out your own
+
+curl --silent --cookie-jar /tmp/cookies -o /dev/null 'http://www.solarmanpv.com/portal/Terminal/TerminalMain.aspx' && \
+curl --cookie "ASP.NET_SessionId=`awk ' END { print $NF }' /tmp/cookies`"  'http://www.solarmanpv.com/portal/AjaxService.ashx?ac=upTerminalMain&psid=5432'
 
